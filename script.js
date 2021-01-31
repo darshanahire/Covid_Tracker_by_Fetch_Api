32de48719537690a780a63da9a461279602a80cb
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-01-31 22:05:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let url = "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+var html ='';
+fetch(url).then(response => response.json()).then(data => {
+    // console.log(data);
+    
+    var mydata = Array.from(data)
+    
+        mydata.forEach(display)
+}).catch(error => {
+    console.log(error);
+})
+
+function display(element,index) {
+    html+=`<div class="col-md-4" id="table">
+    <h3 style="text-align: center">${element.country}</h3>
+    <table  class="table table-bordered border-dark" style="text-align: center">
+    <thead>
+        <tr id="head">
+            <th class="table-success border-dark"scope="row">Infected</th>
+            <th class="table-success border-dark"scope="row">Recoverd</th>
+            <th class="table-success border-dark"scope="row">Deceased</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>${element.infected}</td>
+            <td>${element.recovered}</td>
+            <td>${element.deceased}</td>
+        </tr>
+    </tbody>
+</table>
+</div>`
+let table = document.getElementById("mytable")
+table.innerHTML=html
+}
---
+++ script2.js
@@ -0,0 +1,42 @@
+let contener = document.getElementById("contener")
+
+let url = "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+var html ='';
+fetch(url).then(response => response.json()).then(data => {
+    console.log(data);
+    
+    var mydata = Array.from(data)
+    
+        mydata.forEach(display)
+}).catch(error => {
+    console.log(error);
+})
+
+function display(element,index) {
+    html+=`
+    <div class="accordion" id="accordionExample" style="width:50%;margin:2px auto;">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index+1}"
+                aria-expanded="true" aria-controls="collapseOne" style="align-item: center;">${index+1}. ${element.country}
+              </button>
+            </h2>
+            <div id="collapse${index+1}" class="accordion-collapse collapse" aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+              <b>Country Name:</b> ${element.country}<br>
+              <b>Total Infected Persons:</b> ${element.infected}<br>
+              <b>Total Recoverd Persons:</b> ${element.recovered}<br>
+              <b>Total Deceased Persons:</b> ${element.deceased}<br>
+              <b>Total Tested Persons:</b> ${element.tested}<br>
+              <b>Last Data Update Date:</b> ${element.lastUpdatedApify}<br>
+              <a href=${element.sourceUrl} target="_blank">more info</a>
+              </div>
+            </div>
+          </div>
+        </div>`
+    
+    
+        let table = document.getElementById("finaltable")
+        table.innerHTML=html
+      }
